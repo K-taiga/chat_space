cf142915118ee7bc1ec3e22f33ad537df1a1b88b
Kobatai <taiga1983346@gmail.com>
2019-02-19 20:35:44 +0900

    非同期通信の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,46 @@
 $(function() {
   function buildHTML(message) {
-    var html = `<div class= upper-message>
-                  <div class=upper-message__user-name>
-                    ${message.user_name}
+    var image = message.image.url ? `<img src='${message.image.url}'> ` : ''
+    var html = `<div class = "message" data-id=${message.id}>
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class = "upper-message__date">
+                      ${message.date}
+                    </div>
                   </div>
-                  <div class=upper-message__date>
-                    ${message.date}
-                  </div>
-                  <div class=lower-meesage>
-                    <p class= lower-message__content>
+                  <p class = "lower-message__content">
                     ${message.content}
-                    </p>
-                    ${ message.image }
-                  </div>
+                    ${image}
+                  </p>
                 </div>`
+    console.log(html);
     return html;
   }
-
-  $(`.chat-main__footer-form`).on(`submit`,function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+  $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        console.log(data);
+        var html = buildHTML(data);
+        $('.chat-main__body').append(html);
+        $(".chat-main__body").animate({scrollTop:$('.chat_main-body')});
+        $('.chat-main__footer-body-message').val('');
+        $(".chat-main__footer-form").removeAttr("disabled");
+      })
+      .fail(function(){
+        alert('error');
+      })
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-main__body').append(html);
-      $('.chat-main__body').animate({scrollTop:$('.chat-main__body')});
-      $('.chat-main__footer-body-message').val('');
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
+});
 
--- app/views/messages/index.json.jbuilder
+++
@@ -1,6 +0,0 @@
-json.array! @new_messages |new_message|
-  json.user_name new_message.user.name
-  json.date new_message.created_at
-  json.content new_message.content
-  json.image new_message.image
-end
