d3b617cb85dbb7911ffe674c6c61f2ba9f7c39b6
Kobatai <taiga1983346@gmail.com>
2019-02-23 16:22:38 +0900

    インクリメンタルサーチ実装

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,51 @@
+$(document).on('turbolinks:load', function() {
+
+  var search_list = $("#user-search-result");
+
+  function appendUserName(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">
+                  ${user.name}
+                  </p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">
+                    追加
+                  </a>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUserName(fail_comment) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">
+                  ${fail_comment}
+                  </p>
+                </div>`
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    if(input!==""){
+      $.ajax({
+        type: "GET",
+        url: "/users",
+        data: { keyword: input },
+        dataType: "json"
+      })
+      .done(function(users) {
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUserName(user);
+          });
+        }
+        else {
+          appendNoUserName("一致する名前はありません");
+        }
+      })
+      .fail(function() {
+        alert('名前検索に失敗しました');
+      })
+    }
+  });
+});
