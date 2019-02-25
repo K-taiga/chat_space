9297e1fb8f91e5ec27af7dddbabded84afc18c02
Kobatai <taiga1983346@gmail.com>
2019-02-25 21:05:34 +0900

    finish 自動更新

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,17 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json{@new_messages = @messages.where('id > ?', params[:id])}
+    end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
+        format.html { redirect_to action: :index }
         format.json
       end
     else
